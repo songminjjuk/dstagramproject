e9600063a48cda9c48a62ec23b51375c22e0bc66
songminjjuk <102357118+songminjjuk@users.noreply.github.com>
2023-01-10 03:18:49 +0900

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
     path('', include('photo.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
