73eba82952888cfc4ae1dfa07674b156af48bb19
songminjjuk <102357118+songminjjuk@users.noreply.github.com>
2023-01-15 23:36:22 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/res/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'photo/static/images') # media
 
 LOGIN_REDIRECT_URL = '/'
 
