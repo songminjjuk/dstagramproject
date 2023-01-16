625d00cf375cf5ec035d618c334c779e8773a8c3
songminjjuk <102357118+songminjjuk@users.noreply.github.com>
2023-01-17 03:16:53 +0900

    Update models.py

--- photo/models.py
+++ photo/models.py
@@ -1,13 +1,17 @@
-from django.contrib.auth.models import User
+from datetime import timezone
+
+from django.conf import settings
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
 class Photo(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
     text = models.TextField(blank=True)
-    image = models.ImageField(upload_to= 'timeline_photo/%Y/%m/%d')
+    image = models.ImageField(upload_to='timeline_photo/%Y/%m/%d')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
@@ -15,13 +19,26 @@ class Photo(models.Model):
     favorite = models.ManyToManyField(User, related_name='favorite_post', blank=True)
 
     def __str__(self):
-        return "text : "+self.text
+        return "text : " + self.text
 
     class Meta:
         ordering = ['-created']
-
 
     def get_absolute_url(self):
         return reverse('photo:detail', args=[self.id])
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    profile_photo = models.ImageField(blank=True)  # 값을 채워넣지 않아도 되는 속성.
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Photo, on_delete=models.CASCADE)
+    content = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'comments'
