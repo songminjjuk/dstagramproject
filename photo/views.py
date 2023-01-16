44a2979300de2b945285b214c77fde6003f33e22
songminjjuk <102357118+songminjjuk@users.noreply.github.com>
2023-01-17 03:16:24 +0900

    Update views.py

--- photo/views.py
+++ photo/views.py
@@ -2,13 +2,21 @@ from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView, CreateView, DeleteView
 from django.views.generic.detail import DetailView
+from accounts.forms import UserForm
+from django.contrib.auth.models import User
+import accounts.views
 from .models import Photo
-
-
+from .forms import ProfileForm, CommentForm
+from django.views import View
 from django.http import HttpResponseRedirect
-
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
-
+from datetime import timezone
+import json
+from json.decoder import JSONDecodeError
+from django.views import View
+from django.http import JsonResponse
+from .models import Comment
 
 class PhotoList(ListView):
     model = Photo
@@ -152,8 +160,63 @@ class PhotoMyList(ListView):
             messages.warning(request, '로그인을 먼저하세요')
             return HttpResponseRedirect('/')
         return super(PhotoMyList, self).dispatch(request, *args, **kwargs)
-class Profile(ListView):
-    def get(self, request):
-        user = accounts.forms.UserForm(request.GET)
-        return render(request, 'layout/base.html', {'user': user} )
 
+
+class Profile(DetailView):
+    context_object_name = 'profile_user'  # model로 지정해준 User모델에 대한 객체와 로그인한 사용자랑 명칭이 겹쳐버리기 때문에 이를 지정해줌.
+    model = User
+    template_name = 'photo/photo_mylist.html'
+
+class ProfileUpdateView(View): # 간단한 View클래스를 상속 받았으므로 get함수와 post함수를 각각 만들어줘야한다.
+    # 프로필 편집에서 보여주기위한 get 메소드
+    def get(self, request):
+        user = get_object_or_404(User, pk=request.user.pk)  # 로그인중인 사용자 객체를 얻어옴
+
+        if hasattr(user, 'profile'):  # user가 profile을 가지고 있으면 True, 없으면 False (회원가입을 한다고 profile을 가지고 있진 않으므로)
+            profile = user.profile
+            profile_form = ProfileForm(initial={
+                'profile_photo': profile.profile_photo,
+            })
+        else:
+            profile_form = ProfileForm()
+
+        return render(request, 'photo/profile_update.html', {"profile_form": profile_form})
+
+    def post(self, request):
+        u = User.objects.get(id=request.user.pk)  # 로그인중인 사용자 객체를 얻어옴
+        user_form = UserForm(request.POST, instance=u)  # 기존의 것의 업데이트하는 것 이므로 기존의 인스턴스를 넘겨줘야한다. 기존의 것을 가져와 수정하는 것
+        # User 폼
+        if user_form.is_valid():
+            user_form.save()
+
+        if hasattr(u, 'profile'):
+            profile = u.profile
+            profile_form = ProfileForm(request.POST, request.FILES, instance=profile)  # 기존의 것 가져와 수정하는 것
+        else:
+            profile_form = ProfileForm(request.POST, request.FILES)  # 새로 만드는 것
+
+        if profile_form.is_valid():
+            profile = profile_form.save(commit=False)  # 기존의 것을 가져와 수정하는 경우가 아닌 새로 만든 경우 user를 지정해줘야 하므로
+            profile.user = u
+            profile.save()
+
+        return redirect('/', pk=request.user.pk)  # 수정된 화면 보여주기
+
+def comment_write(request, board_id):
+    comment_write = CommentForm(request.POST)
+    user_id = request.session['user']
+    user = User.objects.get(pk=user_id)
+    if comment_write.is_valid():
+        comments = comment_write.save(commit=False)
+        comments.post = get_object_or_404(Photo, pk=board_id)
+        comments.user = user
+        comments.content = request.POST['body']
+        comments.created_at = timezone.now()
+        comments.save()
+    return redirect('board_detail', board_id)
+
+def board_detail(request, pk):
+    board = get_object_or_404(Photo, pk=pk)
+    comments = CommentForm()
+    comment_view = Comment.objects.filter(post=pk)
+    return render(request, 'board_detail.html',{'board':board, 'comments':comments, 'comment_view':comment_view})
